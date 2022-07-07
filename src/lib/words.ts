31fb99d53a9cf38cf84dd7bbdd5d191b0dd583ae
Elizabeth Foster <elizabeth@rootedfarmers.com>
2022-07-07 18:24:33 -0400

    first commit

---
+++ src/lib/words.ts
@@ -0,0 +1,34 @@
+import { WORDS } from '../constants/wordlist'
+import { VALIDGUESSES } from '../constants/validGuesses'
+
+export const isWordInWordList = (word: string) => {
+  return (
+    WORDS.includes(word.toLowerCase()) ||
+    VALIDGUESSES.includes(word.toLowerCase())
+  )
+}
+
+export const isWinningWord = (word: string) => {
+  return solution === word
+}
+
+export const getLatinDefinition = (word: string) => {
+  return `https://www.latindictionary.io/words/?word=${word}`
+}
+
+export const getWordOfDay = () => {
+  // January 1, 2022 Game Epoch
+  const epochMs = 1641013200000
+  const now = Date.now()
+  const msInDay = 86400000
+  const index = Math.floor((now - epochMs) / msInDay)
+  const nextday = (index + 1) * msInDay + epochMs
+
+  return {
+    solution: WORDS[0].toUpperCase(),
+    solutionIndex: 0,
+    tomorrow: nextday,
+  }
+}
+
+export const { solution, solutionIndex, tomorrow } = getWordOfDay()
