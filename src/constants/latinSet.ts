e202d1b297afd7d283c392b3a07c2d7f79c1ebfe
Elizabeth Foster <elizabeth@rootedfarmers.com>
2022-07-08 08:44:12 -0400

    easy vs hard mode 1.3

--- src/constants/latinSet.ts
+++ src/constants/latinSet.ts
@@ -4,11 +4,9 @@ export function isLatin(answer: string) {
   const latinSet = new Set<string>()
 
   for (let i = 0; i < VALIDGUESSES.length; i++) {
-    latinSet.add(VALIDGUESSES[i])
+    latinSet.add(VALIDGUESSES[i].toUpperCase())
   }
-
-  console.log(latinSet.has(answer))
-  if (!latinSet.has(answer)) {
+  if (latinSet.has(answer)) {
     return true
   } else {
     return false
